cb54fb4e3563c201cc7ac345ba211fc17b6e832b
konapaul <www.arpita.bd@gmail.com>
2023-03-16 17:11:33 +0600

    Uva problems p1005, p10071, p11172

---
+++ p10055.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    long long int a, b;
+
+    while(scanf("%lld%lld", &a, &b) != EOF){
+    
+    if( a < b)
+        printf("%lld\n", b - a);
+    
+    else{
+        printf("%lld\n", a - b);
+    }
+}
+return 0;
+}
